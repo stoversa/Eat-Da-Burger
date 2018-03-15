33a661fe85775253df8eb15398ec1ac535aee933
stoversa <stoversa@gmail.com>
2018-03-14 21:17:40 -0400

    Initial Commit

---
+++ controllers/burgersController.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
+
+router.get("/", (req, res) => {
+    burger.all(function (data) {
+        const hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,11 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+}
+
+module.exports = burger;

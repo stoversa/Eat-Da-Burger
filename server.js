ed26b8447feaa5654c1974891895fb2bc1dbf6ea
stoversa <stoversa@gmail.com>
2018-03-14 21:18:04 -0400

    Basic server layout created

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+//connection for Heroku
+const PORT = process.env.PORT || 8080;
+//express
+const app = express();
+app.use(express.static("public"));
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//controller
+const routes = require("./controllers/burgersController.js");
+app.use(routes);
+
+//listener
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});

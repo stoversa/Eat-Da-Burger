57fa16b6dc4b509e3cf92dcb5c8732df1becdda1
stoversa <stoversa@gmail.com>
2018-03-18 13:59:54 -0400

    Fixed variable mismatch

--- config/orm.js
+++ config/orm.js
@@ -10,7 +10,7 @@ var orm = {
         });
     },
     insertOne: function (table, column, value, cb) {
-        let queryString = `INSERT INTO ${table} ${column.toString()} VALUES ("${value.toString()})";`;
+        let queryString = `INSERT INTO ${table} (${column.toString()}) VALUES ("${value.toString()}");`;
         console.log(queryString);
 
         connection.query(queryString, function (err, result) {
@@ -18,8 +18,8 @@ var orm = {
             cb(result);
         });
     },
-    updateOne: function (table, column, condition, cb) { 
-        let queryString = `UPDATE ${table} SET ${column.toString()} WHERE ${condition.toString()};`;
+    updateOne: function (table, column, value, condition, cb) { 
+        let queryString = `UPDATE ${table.toString()} SET ${column.toString()} = ${value} WHERE ${condition.toString()};`;
         console.log(queryString);
 
         connection.query(queryString, function (err, result) {

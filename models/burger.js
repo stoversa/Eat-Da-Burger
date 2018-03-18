b2170d88f69f081a6113b9929f47cca8e13077c6
stoversa <stoversa@gmail.com>
2018-03-18 14:00:38 -0400

    Updated to ensure working API

--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,17 @@ const orm = require("../config/orm.js");
 
 const burger = {
     all: function (cb) {
-        orm.all("burgers", function (res) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    create: function (value, cb) {
+        orm.insertOne("burgers", "name", value, function (res) {
+            cb(res);
+        });
+    },
+    update: function (id, cb) {
+        orm.updateOne("burgers", "is_eaten", true, "id = " + id, function (res) {
             cb(res);
         });
     },

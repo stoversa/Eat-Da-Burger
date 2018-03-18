b54f1162e2465723d87a36750ffcfa38272d7e6f
stoversa <stoversa@gmail.com>
2018-03-18 12:49:39 -0400

    Minor typographical change.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 //connection for Heroku
 const PORT = process.env.PORT || 8080;
+
 //express
 const app = express();
 app.use(express.static("public"));
